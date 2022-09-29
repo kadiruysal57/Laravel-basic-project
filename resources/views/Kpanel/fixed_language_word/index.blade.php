44b0a2a397bc7b2fc48bbb8e97710d238131b246
ClinicCenterDev <developers@cliniccenter.co.uk>
2022-09-30 00:19:43 +0300

    fixed language word bugfix

--- resources/views/Kpanel/fixed_language_word/index.blade.php
+++ resources/views/Kpanel/fixed_language_word/index.blade.php
@@ -11,9 +11,7 @@
         <div class="col-12">
             <div class="card">
                 <header class="card-header">
-                    @foreach($data as $d)
-                        {{__('fixed_word.'.$d)}}
-                    @endforeach
+
                     <h4 class="card-title">{{__('fixed_word.fixed_word')}}</strong></h4>
                 </header>
                 <div class="card-body">
